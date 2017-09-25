98f1bb53f1e859dd6e5055b41d9cd176542a4ef1
Bartosz Jedynak <Bartosz.Jedynak@reedbusiness.com>
2017-09-25 16:19:07 +0200

    new boilerplate code

---
+++ frontend/src/components/SignupForm.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+class SignupForm extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            username: ''
+        };
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+onChange(e){
+
+    this.setState({[ e.target.name]: e.target.value });
+}
+onSubmit(e) {
+    e.preventDefault();
+    console.log(this.state);
+}
+
+    render() {
+        return (
+            
+          <form onSubmit={this.onSubmit}>
+              <h1> Welcome </h1>
+
+              <div className="form-group">
+                  <label className="control-label"> Username </label>
+                  <input
+                  value={this.state.username}
+                  onChange={this.onChange}
+                    type="text"
+                    name="username"
+                    className="from-control"
+                    />
+              </div>
+         
+
+
+          <div className="from-group">
+              <button className="btn btn-primary btn-lg">
+                    Sing Up
+                  </button>
+
+            
+          </div>
+
+          </form>
+        );
+    }
+}
+
+export default SignupForm;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
-registerServiceWorker();
+import SignupForm from './components/SignupForm';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <SignupForm />
+  </Provider>
+  , document.querySelector('#app'));
