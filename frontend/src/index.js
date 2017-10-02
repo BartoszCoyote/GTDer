5970c7da4fb5fb60ba2a199ce4279d00981eacf1
Bartosz <BartoszCoyote@gmail.com>
2017-10-02 20:48:56 +0200

    clean up

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,25 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import SignupPage from './components/singup/SignupPage';
+
 import reducers from './reducers';
 import App from './components/app';
-import NavigationBar from './components/NavigationBar';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <BrowserRouter>
-    <div>
-    <NavigationBar />
-    <Switch>
-    <Route path="/signup" component={SignupPage}/>
-    <Route path="/" component={App} />
-    </Switch>
-    </div>
-    </BrowserRouter>
+     <App />
     </Provider>
   , document.querySelector('#app'));
