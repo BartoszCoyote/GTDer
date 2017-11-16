6f06771f4edef48e3b796bc8375d19f94b14582c
Bartosz <BartoszCoyote@gmail.com>
2017-11-16 17:30:27 +0100

    add redux devtools to project

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { createStore, applyMiddleware } from 'redux';
+import {BrowserRouter} from 'react-router-dom';
+import {applyMiddleware, createStore} from 'redux';
 import reduxThunk from 'redux-thunk';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 
 import reducers from './reducers';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import { AUTH_USER } from './action/types';
+import {AUTH_USER} from './action/types';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
-const store = createStoreWithMiddleware(reducers);
+const store = createStoreWithMiddleware(reducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const user = localStorage.getItem('token');
 
