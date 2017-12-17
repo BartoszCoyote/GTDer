7cd1e4fb6e22e9a83d4b208a2b2c03594d324496
Bartosz <BartoszCoyote@gmail.com>
2017-12-18 00:37:44 +0100

    add devtools to pom, add project repo

---
+++ backend/src/main/java/pl/jedynakbartosz/backend/project/ProjectRepository.java
@@ -0,0 +1,14 @@
+package pl.jedynakbartosz.backend.project;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project,Long> {
+
+    Project findByName(String name);
+
+    Project findById(String id);
+
+    void deleteById(String id);
+}
