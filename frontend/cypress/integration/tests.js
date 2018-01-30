c95bb79608570f65049f92b1ce6c4a5a82087c0d
Bartosz <BartoszCoyote@gmail.com>
2018-01-30 15:32:22 +0100

    add tests cypress

--- frontend/cypress/integration/tests.js
+++ frontend/cypress/integration/tests.js
@@ -1,17 +1,105 @@
-describe('Kitchen Sink', function () {
-  it('.should() - assert that <title> is correct', function () {
+describe('GTDer', function () {
+  it('assert frontend is up', function () {
     // https://on.cypress.io/visit
     cy.visit('localhost:3000')
+    cy.title().should('include', 'React App')
+    cy.location().should(function (location) {
+      expect(location.href).to.eq('http://localhost:3000/')
+    })
 
-    // Here we've made our first assertion using a '.should()' command.
-    // An assertion is comprised of a chainer, subject, and optional value.
 
-    // https://on.cypress.io/should
-    // https://on.cypress.io/and
 
-    // https://on.cypress.io/title
-    cy.title().should('include', 'Kitchen Sink')
-    //   ↲               ↲            ↲
-    // subject        chainer      value
   })
-}
+  it('sign in page', function () {
+    cy.get('.center').click()
+    cy.contains('Sign in').click()
+
+    cy.location().should(function (location) {
+      expect(location.href).to.eq('http://localhost:3000/signin')
+    })
+
+
+
+
+  })
+  it('sign up page', function () {
+    cy.contains('Sign up').click()
+    cy.location().should(function (location) {
+      expect(location.href).to.eq('http://localhost:3000/signup')
+    })
+  })
+
+  it('sign in page, click login without password', function () {
+    cy.contains('Sign in').click()
+    cy.contains('Login').click()
+    cy.get('.text-help').contains('Enter Username')
+
+  })
+  it('sign in page, click login without password', function () {
+    cy.contains('Sign up').click()
+    cy.get('.btn-primary').click()
+    cy.get('.text-help').contains('Enter Username')
+
+  })
+  it('sign in page bad user/password', function () {
+    cy.get('.center').click()
+    cy.contains('Sign in').click()
+
+    cy.location().should(function (location) {
+      expect(location.href).to.eq('http://localhost:3000/signin')
+    })
+
+    cy.get('.form-group').contains('Username:').get('input[type=text]').type('xd')
+
+    cy.get('.form-group').contains('Password:').get('input[type=password]').type('xd')
+    cy.get('.btn-primary').click()
+    cy.get('.info-red').contains('Invalid email or password')
+
+
+
+  })
+  it('sign in page sucessfully', function () {
+    cy.get('.center').click()
+    cy.contains('Sign in').click()
+
+    cy.location().should(function (location) {
+      expect(location.href).to.eq('http://localhost:3000/signin')
+    })
+
+    cy.get('.form-group').contains('Username:').get('input[type=text]').type('admin')
+
+    cy.get('.form-group').contains('Password:').get('input[type=password]').type('admin')
+    cy.get('.btn-primary').click()
+    cy.get('.ButtonModal').contains('Add task')
+    cy.get('.ButtonModal').contains('Add project')
+
+    cy.contains('Sign out').click()
+    cy.get('h1').contains('Good Bye')
+  })
+
+  it('Add Task works', function () {
+    cy.get('.center').click()
+    cy.contains('Sign in').click()
+
+    cy.location().should(function (location) {
+      expect(location.href).to.eq('http://localhost:3000/signin')
+    })
+
+    cy.get('.form-group').contains('Username:').get('input[type=text]').type('admin')
+
+    cy.get('.form-group').contains('Password:').get('input[type=password]').type('admin')
+    cy.get('.btn-primary').click()
+    cy.get('.ButtonModal').contains('Add task')
+    cy.get('.ButtonModal').contains('Add project')
+    cy.get('.ButtonModal').contains('Add task').click()
+    cy.get('.btn-default').click()
+    cy.get('.ButtonModal').contains('Add task').click()
+    cy.get('.form-group').contains('Name:').first().get('input').type('admin')
+
+
+
+
+
+
+  })
+})
